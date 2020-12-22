2a187ed81ea706cca1e33a0a141e4c01067fbb25
rajnisaini <71651783+rajnisiani@users.noreply.github.com>
2020-12-22 19:08:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+var r1,r2,r3;
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
@@ -30,6 +31,15 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
+	
+	r1 = Bodies.rectangle(400,640,200,10)
+	World.add(world, r1);
+
+	r2 = Bodies.rectangle(300,610,10,100)
+	World.add(world, r2);
+
+	r3 = Bodies.rectangle(500,610,10,100)
+	World.add(world, r3);
 
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
@@ -40,13 +50,16 @@ function setup() {
  	World.add(world, ground);
 
 	
+
+
 	Engine.run(engine);
 }
 
 
 function draw() {
-  rectMode(CENTER);
   background(0);
+  rectMode(CENTER);
+
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   drawSprites();
@@ -54,6 +67,12 @@ function draw() {
   if(keyDown(DOWN_ARROW)) {
 	Matter.Body.setStatic(packageBody,false);
 	}	
+	rectMode(CENTER);
+	rect(r1.position.x,r1.position.y,200,10)
+	rectMode(CENTER);
+	rect(r2.position.x,r2.position.y,10,100)
+	rectMode(CENTER);
+	rect(r3.position.x,r3.position.y,10,100)
  
   Engine.update(engine);
 }
